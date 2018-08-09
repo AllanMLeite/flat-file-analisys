3b2507b68ec5f7eaa311ebfa908a8549e43a5931
Allan Moreira Leite <allanmleite@hotmail.com>
2018-08-09 14:57:32 -0300

    projeto spring inicializado

---
+++ src/main/java/com/ilegra/desafiotecnico/DesafioTecnicoApplication.java
@@ -0,0 +1,12 @@
+package com.ilegra.desafiotecnico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTecnicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTecnicoApplication.class, args);
+	}
+}
